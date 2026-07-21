c3d3b0503f46725bb40ea85c95641dc4df4f8072
Abhinav Subramanian <abhinav.subr@gmail.com>
2026-07-21 16:40:36 +0530

    added requirements.txt and .env

--- raspberrypi/send_to_cloud.py
+++ raspberrypi/send_to_cloud.py
@@ -1,6 +1,7 @@
 import serial
 import time
-
+from dotenv import load_dotenv
+import os
 # Initialize the serial connection
 ser = serial.Serial(
     port='/dev/serial0',  # Use '/dev/ttyUSB0' if using USB interface
@@ -8,8 +9,10 @@ ser = serial.Serial(
     timeout=10  # 1 second timeout for read/write operations
 )
 
-# ThingSpeak URL and API key
-URL = "https://api.thingspeak.com/update?api_key=IYJKIB141WMLJ0GJ&"
+
+load_dotenv()
+URL = os.getenv("THINGSPEAK_URL")   
+
 
 def read_from_gsm():
     """Read response from the GSM module."""
@@ -17,9 +20,9 @@ def read_from_gsm():
     response = ""
     start_time = time.time()
 
-    # Wait for data from GSM module
+    # Wait data from gsm module
     while ser.in_waiting == 0:
-        if time.time() - start_time > 10:  # Timeout after 10 seconds
+        if time.time() - start_time > 10: 
             print("Timeout waiting for GSM response.")
             return
 
@@ -33,14 +36,13 @@ def read_from_gsm():
 def send_command(command):
     """Send AT command to the GSM module."""
     ser.write((command + '\r\n').encode('utf-8'))
-    time.sleep(0.2)  # Wait for the GSM module to process the command
+    time.sleep(0.2)  
     read_from_gsm()
 
 def send_sensor_data_to_cloud(temperature, humidity, ph,risk):
     """Send sensor data to the cloud via HTTP GET request."""
     print("Sending sensor data to cloud...")
 
-    # Prepare data payload
     data = "field1="+str(temperature)+"&field2="+str(humidity)+"&field3="+str(ph)+"&field4="+str(risk)
     full_url = URL + data
 
@@ -61,14 +63,13 @@ def setup_gsm():
     """Set up the GSM module."""
     print("Setting up the GSM module...")
     time.sleep(30)  # Wait for the GSM module to boot
-    send_command("AT")  # Check GSM module communication
+    send_command("AT")  
     print("GSM initialized.")
 
 # Main script
 if __name__ == "__main__":
     try:
         setup_gsm()
-        # Example: send dummy data to the cloud
         send_sensor_data_to_cloud(25.69,69.5,42.0)
     except KeyboardInterrupt:
         print("Exiting...")
